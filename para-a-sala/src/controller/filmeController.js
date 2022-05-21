d28040d1a7bb86660b6abc0c193cafc38be498bc
Maysafig <maysafig@gmail.com>
2022-05-21 20:14:55 -0300

    Criação de um arquivo .md com o passo a passo da aula

--- para-a-sala/src/controller/filmeController.js
+++ para-a-sala/src/controller/filmeController.js
@@ -11,7 +11,7 @@ const getAll = (request, response) => {
 
     response.status(200).json([
         {
-            "filmes" : filmesJson
+            "filmes": filmesJson
         }
     ])
 }
@@ -25,15 +25,14 @@ const updateTitle = (request, response) => {
     // guardo o titulo que foi enviado no body da requisiçao (pq body? pq pra put, patch e post eu PRECISO passar um body)
     let novoTitulo = request.body.title
 
-   // preciso filtrar o meu "banco de dados" (nesse caso é o Json mocado) para encontrar o id que o usuário digitou
-   filmeFiltrado = filmesJson.find(filme => filme.id == idRequest)
+    // preciso filtrar o meu "banco de dados" (nesse caso é o Json mockado) para encontrar o id que o usuário digitou
+    filmeFiltrado = filmesJson.find(filme => filme.id == idRequest)
 
     //Uma parte especifica do filme, no caso o título, será alterada com a nova informação enviada (o novo titulo)
     filmeFiltrado.title = novoTitulo
 
     //Mando uma response dizendo que está tudo correto e envio o filme com a alteração
     response.status(200).json([{
-
         "mensagem": "seu filme foi atualizado",
         filmeFiltrado
     }])
@@ -42,7 +41,7 @@ const updateTitle = (request, response) => {
 //Alterar todas as informações do filme buscando pelo id
 const updateMovie = (request, response) => {
 
-    const idRequest = request.params.id 
+    const idRequest = request.params.id
     let filmeRequest = request.body
 
     //o Index Retorna o indice do array no primeiro elemento que for true
@@ -61,40 +60,40 @@ const updateMovie = (request, response) => {
 }
 
 //Deletar um filme por titulo
-const deleteFilme = (request, response ) => {
+const deleteFilme = (request, response) => {
 
     //id que quero deletar
     const idRequest = request.params.id
 
     //Pegar o index(indice) do filme que será deletado
-    const indexFilme = filmesJson.findIndex( filme => filme.id == idRequest)
+    const indexFilme = filmesJson.findIndex(filme => filme.id == idRequest)
 
-    //retira o filme do array de filmes a partir do index indicado
-    //array.splice(index, numero de coisas que serão deletadas, item que vou adicionar)
+    /*retira o filme do array de filmes a partir do index indicado
+    array.splice(index, numero de coisas que serão deletadas, item que vou adicionar)*/
     filmesJson.splice(indexFilme, 1) //pra deletar não precisa adicionar nada
 
     response.status(200).json([{
 
-        "mensagem" : "O filme foi deletado",
-        "deletado" : idRequest,
+        "mensagem": "O filme foi deletado",
+        "deletado": idRequest,
         filmesJson
     }])
 }
 
 //Deletar um filme por titulo
-const deleteFilmePorTitulo = (request, response ) => {
+const deleteFilmePorTitulo = (request, response) => {
 
     //titulo que quero deletar
     const titleRequest = request.query.title.toLowerCase()
 
-    const indexTitle = filmesJson.findIndex( filme => filme.title == titleRequest)
+    const indexTitle = filmesJson.findIndex(filme => filme.title == titleRequest)
 
-    filmesJson.splice(indexTitle,1)
+    filmesJson.splice(indexTitle, 1)
 
     response.status(200).json([{
 
-        "mensagem" : "O filme foi deletado",
-        "deletado" : titleRequest,
+        "mensagem": "O filme foi deletado",
+        "deletado": titleRequest,
         filmesJson
     }])
 }
@@ -107,4 +106,4 @@ module.exports = {
     deleteFilme,
     deleteFilmePorTitulo,
 
-}
+} 
