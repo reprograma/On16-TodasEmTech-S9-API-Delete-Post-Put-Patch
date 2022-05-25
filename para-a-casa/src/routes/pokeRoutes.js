572cd78ff12e6b985d8f24bfe391e8a4a81f7d4e
Carol Alves <prof.carolalves90@gmail.com>
2022-05-25 19:08:34 -0300

    :boom: alterando rota get

--- para-a-casa/src/routes/pokeRoutes.js
+++ para-a-casa/src/routes/pokeRoutes.js
@@ -4,10 +4,10 @@ const express = require("express")
 
 const router = express.Router()
 
-router.get("/lista", controller.getAll)
+router.get("/", controller.getAll)
 router.put("/update/:id", controller.updateAll)
 router.patch("/update/:id", controller.updateStats)
-router.delete("/deletar", controller.deleteTipo)
+router.delete("/deletar/tipo", controller.deleteTipo)
 router.delete("/deletar/:id", controller.deletePokemon)
 
 module.exports = router
