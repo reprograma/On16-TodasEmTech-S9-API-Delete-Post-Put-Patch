80edf93616104990aaf54725a2e2355d9658edfb
Carol Alves <prof.carolalves90@gmail.com>
2022-05-26 15:21:03 -0300

    :ok_hand: atualização na rota de atualizar pokemon

--- para-a-casa/src/routes/pokeRoutes.js
+++ para-a-casa/src/routes/pokeRoutes.js
@@ -5,7 +5,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", controller.getAll)
-router.put("/update/:id", controller.updateAll)
+router.put("/updateAll/:id", controller.updateAll)
 router.patch("/update/:id", controller.updateStats)
 router.delete("/deletar/tipo", controller.deleteTipo)
 router.delete("/deletar/:id", controller.deletePokemon)
