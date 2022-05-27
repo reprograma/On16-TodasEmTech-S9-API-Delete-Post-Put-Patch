8d571520a62534bc1c30ad199502ec54cdd20580
caveroni <veronicapego76@gmail.com>
2022-05-27 20:09:49 -0300

    exercicios da semana

--- para-a-sala/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app") //chamando o arquivo app
-
-const PORT = 1313 //PORTA
-
-//iniciando o servidor
-app.listen(PORT , ()=>{
-    console.log(`eaiiii, to na porta ${PORT}`)
-})
